5d0a468103ad5127c686dd85d065dc962e55fbf1
Darkilen <samuel.arlaud@gmail.com>
2017-01-17 18:50:45 +0100

    add display of tests + clean some code in app.js + fix some bugs

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 "use strict"
+
 const app = angular.module("RandomApp", []);
 app.controller("RandomController", function($scope) {
   $scope.title = "Le jeu du random";
-  $scope.actualLimit = 0;
+  $scope.rules = "Le but du jeu est de trouver un nombre aléatoire compris entre 0 et 1000 en 7 essais";
+  $scope.actualLimit = 1;
+  $scope.list = [];
   $scope.newInit = function(minNumber, maxNumber) {
     if((typeof $scope.minNumber) === 'undefined')
     {
@@ -16,42 +19,64 @@ app.controller("RandomController", function($scope) {
     {
       $scope.limit = 7;
     }
-    console.log($scope.minNumber, $scope.maxNumber, $scope.limit);
-    $scope.goodNumber = Math.floor(Math.random() * $scope.maxNumber + $scope.minNumber);
-    console.log($scope.goodNumber);
-    document.getElementById('selectNumber').className = "activated form-horizontal";
-    document.getElementById('init').className = "disabled"
+    if($scope.maxNumber>$scope.minNumber){
+      $scope.rules = "Le but du jeu est de trouver un nombre aléatoire compris entre "+$scope.minNumber+" et "+$scope.maxNumber+" en "+$scope.limit+" essais";
+      $scope.essai = $scope.limit;
+      $scope.goodNumber = Math.floor(Math.random() * ($scope.maxNumber-$scope.minNumber) + $scope.minNumber);
+      console.log($scope.goodNumber);
+      document.getElementById("newNumberInput").setAttribute("max", $scope.maxNumber);
+      document.getElementById("newNumberInput").setAttribute("min", $scope.minNumber);
+      document.getElementById("selectNumber").classList.toggle( "disabled");
+      document.getElementById("init").classList.toggle("disabled");
+      document.getElementById("list").classList.toggle("disabled");
+      document.querySelector("h3").classList.toggle("disabled");
+      document.querySelector("br").remove();
+      $scope.error = "";
+    }else{
+      $scope.error = "Le minimum doit être inférieur au maximum !";
+    }
   }
+  
   $scope.testNumber = function(number) {
-    console.log($scope.limit);
     if((typeof $scope.goodNumber) === 'number')
     {
-      if($scope.actualLimit<$scope.limit){
-        if (number < $scope.goodNumber) {
-          console.log("C'est plus !");
-          $scope.actualLimit++;
-
-        } else if (number > $scope.goodNumber) {
-          console.log("C'est moins !");
-          $scope.actualLimit++;
+      if((typeof number) === 'number'){
+        if($scope.actualLimit<$scope.limit){
+          if (number < $scope.goodNumber) {
+            $scope.list.push("C'est plus ! (Ton nombre : "+number+")");
+            $scope.actualLimit++;
+            $scope.essai--;
+          } else if (number > $scope.goodNumber) {
+            $scope.list.push("C'est moins ! (Ton nombre : "+number+")");
+            $scope.actualLimit++;
+            $scope.essai--;
+          } else {
+            $scope.list.push("Vous avez trouvé la bonne réponse : "+$scope.goodNumber);
+            document.getElementById('reset').classList.toggle("disabled");
+            document.getElementById("newNumberInput").setAttribute("disabled", true);
+            $scope.essai--;
+          }
         } else {
-          console.log("Vous avez trouvé la bonne réponse :", $scope.goodNumber);
-          document.getElementById('reset').className = "activated btn btn-default";
+          $scope.list.push("Vous avez perdu, la bonne réponse était : "+$scope.goodNumber+" (Ton nombre : "+number+")");
+          document.getElementById('reset').classList.toggle("disabled");
+          document.getElementById("newNumberInput").setAttribute("disabled", true);
+          $scope.essai--;
         }
       }
-      else {
-        console.log("Vous avez perdu, la réponse était :", $scope.goodNumber);
-        document.getElementById('reset').className = "activated btn btn-default";
-      }
-    }
-    else {
+    } else {
       alert("Error !")
     }
     $scope.newNumber = null;
   }
+
   $scope.reset = function() {
-    $scope.actualLimit = 0;
-    document.getElementById('reset').className = "disabled";
+    $scope.actualLimit = 1;
+    document.getElementById('reset').classList.toggle("disabled");
     $scope.newNumber = null;
+    $scope.list = [];
+    $scope.essai = $scope.limit;
+    $scope.goodNumber = Math.floor(Math.random() * ($scope.maxNumber-$scope.minNumber) + $scope.minNumber);
+    console.log($scope.goodNumber);
+    document.getElementById("newNumberInput").removeAttribute("disabled");
   }
 })
